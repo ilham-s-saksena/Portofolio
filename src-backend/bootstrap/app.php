e593094e5a3eef3bfa3ecddc0b31334bf954ab9e
Ilham Sidik <ilhamsidikc@gmail.com>
2024-03-18 02:49:47 +0700

    add laravel, preparing backend machine

---
+++ src-backend/bootstrap/app.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/app.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+        // authroute: __DIR__.'/../routes/authroute.php',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
