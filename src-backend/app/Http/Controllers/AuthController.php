e957ef62a414f4ce5229b37cd531ed508fe16234
Ilham Sidik <ilhamsidikc@gmail.com>
2024-03-18 10:25:56 +0700

    first push on new branch

---
+++ src-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json(['error' => $validate->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+
+        $token = $user->createToken('PortoApp');
+
+        return response()->json(
+            [
+                'user' => $user, 
+                'token' => $token->plainTextToken,
+                'access_token' => $token->accessToken
+            ], 
+            201
+        );
+    }
+}
