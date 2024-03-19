407d3e1e761eeaf1d9f994baade7adb56e17cd76
Ilham Sidik <ilhamsidikc@gmail.com>
2024-03-19 15:48:48 +0700

    push from c panel and configure it

--- src-backend/database/migrations/2024_03_17_195754_create_personal_access_tokens_table.php
+++ src-backend/database/migrations/2024_03_17_195754_create_personal_access_tokens_table.php
@@ -13,13 +13,19 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            // $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
+    
+            $table->unsignedBigInteger('tokenable_id');
+            $table->string('tokenable_type', 200);
+
+            // Tambahkan indeks manual
+            $table->index(['tokenable_type', 'tokenable_id']);
         });
     }
 
